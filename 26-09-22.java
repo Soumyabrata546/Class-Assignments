5469ba0d0e05dc194e48b058a69056fb513d1273
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-09-26 12:07:28 +0530

    Add files via upload

---
+++ 26-09-22.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class a{
+public static void main(String args[]){
+System.out.print("Multiplication Result="+2.50f*5.50f);
+}
+}
+class b{
+public static void main(String args[]){
+System.out.print("Addition Result="+(5+6));
+}
+}
+class c{
+public static void main(String args[]){
+System.out.print("Mul Result="+(5*6));
+}
+}
+
+class d{
+public static void main(String args[]){
+System.out.print("Enter your name=");
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+System.out.print("Entered string="+s);
+}
+}
