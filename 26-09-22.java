08b26328b790547d5a946dc5233cce803bada1ab
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-09-26 12:52:53 +0530

    Update 26-09-22.java

--- 26-09-22.java
+++ 26-09-22.java
@@ -1,20 +1,23 @@
 import java.util.*;
+//Multiplication of two float numbers
 class a{
 public static void main(String args[]){
 System.out.print("Multiplication Result="+2.50f*5.50f);
 }
 }
+//Add two numbers
 class b{
 public static void main(String args[]){
 System.out.print("Addition Result="+(5+6));
 }
 }
+//Multiplication of two numbers
 class c{
 public static void main(String args[]){
 System.out.print("Mul Result="+(5*6));
 }
 }
-
+//Take input 
 class d{
 public static void main(String args[]){
 System.out.print("Enter your name=");
