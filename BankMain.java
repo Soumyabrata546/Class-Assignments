9df2ba372e1bab88e0b91a5ff44587d5f18030be
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-10-11 11:27:36 +0530

    Add files via upload

---
+++ BankMain.java
@@ -0,0 +1,31 @@
+package HandsOn_Lab;
+/*
+ * Consider a scenario where Bank is a class that provides 
+functionality to get the rate of interest. 
+However, the rate of interest varies according to banks. 
+For example, SBI, ICICI and AXIS banks could 
+provide 8%, 7%, and 9% rate of interest.
+ */
+class Bank{
+	int  getRate(String s) {
+		if(s=="sbi") {
+			return 8;
+		}
+		else if(s=="icici")
+			return 7;
+		else if(s=="axis")
+			return 9;
+		else
+			return 0;
+	}
+}
+public class BankMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank b=new Bank();
+		int rate=b.getRate("icici");
+		System.out.println("Rate of interest= "+rate+"%");
+	}
+
+}
